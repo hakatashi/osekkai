a76187f941cdba84cec9be366d18fd183dd6756c
Koki Takahashi <hakatasiloving@gmail.com>
2018-09-21 15:53:57 +0900

    Install eslint

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+	root: true,
+	env: {
+		node: true,
+		mocha: true,
+	},
+	extends: [
+		'@hakatashi',
+	],
+	// add your custom rules here
+	rules: {
+		'max-params': 'off',
+		'no-underscore-dangle': 'off',
+	},
+	globals: {},
+};
