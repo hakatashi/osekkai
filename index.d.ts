9692e88cff0b494f7776df627a2f80e9f681aee3
Koki Takahashi <hakatasiloving@gmail.com>
2018-10-04 18:21:23 +0900

    Export type declarations in index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -3,72 +3,72 @@
 
 export = osekkai;
 
-interface PlainToken {
-	type: 'plain';
-	text: string;
-}
-
-interface UprightToken {
-	type: 'upright';
-	text: string;
-	original: string;
-}
-
-interface AlterToken {
-	type: 'alter';
-	text: string;
-	original: string;
-}
-
-interface MarginToken {
-	type: 'margin';
-	text: string;
-	original: string;
-	length: number;
-}
-
-type Token = PlainToken | UprightToken | AlterToken | MarginToken;
-
-interface Chunk {
-	getText(): string;
-	tokens: Token[];
-}
-
-interface OsekkaiOptions {
-	converters?: 'default' | 'vectical' | ConverterOptionHash;
-	joinableTokens?: string[];
-}
-
-type ConverterTypes =
-	'exclamations'
-	| 'numbers'
-	| 'alphabetUpright'
-	| 'alphabetMargin'
-	| 'quotations'
-	| 'dashes';
-type FormatterTypes = 'plain' | 'object' | 'aozora';
-
-interface ConverterOptions {
-	length?: number;
-}
-type ConverterOptionHash = {
-	[converter in ConverterTypes]?: ConverterOptions | true;
-};
-
-interface FormatterOptions {
-	entities?: 'aozora' | 'publishing' | {[token: string]: string};
-}
-
-declare class Osekkai {
-	constructor(chunks: string | string[], options?: OsekkaiOptions);
-	convert(converters: ConverterOptionHash | ConverterTypes, options?: ConverterOptions): Osekkai;
-	format(formatter: FormatterTypes, options?: FormatterOptions): Token[];
-	substr(start: number, length: number): Chunk[];
-}
-
-declare function osekkai(chunks: string | string[], options?: OsekkaiOptions): Osekkai;
+declare function osekkai(chunks: string | string[], options?: osekkai.OsekkaiOptions): osekkai.OsekkaiClass;
 
 declare namespace osekkai {
 	export const defaultConfig: OsekkaiOptions;
-	export const Osekkai: Osekkai;
+	export const Osekkai: OsekkaiClass;
+
+	export interface PlainToken {
+		type: 'plain';
+		text: string;
+	}
+
+	export interface UprightToken {
+		type: 'upright';
+		text: string;
+		original: string;
+	}
+
+	export interface AlterToken {
+		type: 'alter';
+		text: string;
+		original: string;
+	}
+
+	export interface MarginToken {
+		type: 'margin';
+		text: string;
+		original: string;
+		length: number;
+	}
+
+	export type Token = PlainToken | UprightToken | AlterToken | MarginToken;
+
+	export interface Chunk {
+		getText(): string;
+		tokens: Token[];
+	}
+
+	export interface OsekkaiOptions {
+		converters?: 'default' | 'vectical' | ConverterOptionHash;
+		joinableTokens?: string[];
+	}
+
+	export type ConverterTypes =
+		'exclamations'
+		| 'numbers'
+		| 'alphabetUpright'
+		| 'alphabetMargin'
+		| 'quotations'
+		| 'dashes';
+	export type FormatterTypes = 'plain' | 'object' | 'aozora';
+
+	export interface ConverterOptions {
+		length?: number;
+	}
+	export type ConverterOptionHash = {
+		[converter in ConverterTypes]?: ConverterOptions | true;
+	};
+
+	export interface FormatterOptions {
+		entities?: 'aozora' | 'publishing' | {[token: string]: string};
+	}
+
+	export class OsekkaiClass {
+		constructor(chunks: string | string[], options?: OsekkaiOptions);
+		convert(converters: ConverterOptionHash | ConverterTypes, options?: ConverterOptions): OsekkaiClass;
+		format(formatter: FormatterTypes, options?: FormatterOptions): Token[];
+		substr(start: number, length: number): Chunk[];
+	}
 }
